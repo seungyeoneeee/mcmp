2d999441e2dcb5b290b43d1b7dfc4ca2df9219b8
이승연 <sylee1274@mz.co.kr>
2024-07-26 18:45:45 +0900

    Setting: libarary install & css config setting & tsconfig setting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,11 @@
 module.exports = () => ({
   plugins: [
-    require('tailwindcss')('./tailwind.config.cjs'),
-    // require('potscss-easy-import')([]),
-    require('postcss-hexrgba'),
-    require('postcss-mixins'),
-    require('postcss-conditionals'),
-    require('postcss-nested'),
+    require('tailwindcss')('./tailwind.config'),
+    // require('potscss-easy-import'),
+    // require('postcss-hexrgba'),
+    // require('postcss-mixins'),
+    // require('postcss-conditionals'),
+    // require('postcss-nested'),
     // require('postcss-simple-vars')(
     //   variables() {
     //     return require('./src/style.css')
@@ -13,6 +13,6 @@ module.exports = () => ({
     // ),
     require('autoprefixer'),
     // require('postcss-loader'),
-    require('postcss-preset-env')({ stage: 3 }),
+    // require('postcss-preset-env')({ stage: 3 }),
   ],
 });
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const mirinaeTailwindConfig = require('@cloudforet-test/mirinae/tailwind.config.cjs');
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{vue,js,ts}'],
-  theme: mirinaeTailwindConfig.theme,
-  variants: mirinaeTailwindConfig.variants,
-  plugins: mirinaeTailwindConfig.plugins,
-};
